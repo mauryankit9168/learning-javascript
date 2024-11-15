788885e5a27a035f843e92235316a97c420569d2
Ankit Maurya <ankit14082004@gmail.com>
2024-11-15 17:49:20 +0000

    container confing done

---
+++ basic/test2.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) //we are using nodejs, not browser
+console.log(3+3) // code must be readable
+
+//DATA TYPES
+    /*
+        number = 2 to power 53
+        bigint
+        string 
+        boolean
+        null
+        undefined
+        symbol=unique
+    */ 
