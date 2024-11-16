29367e07efb9cee5e1a35f93b8f675e5d076a6d4
Ankit Maurya <ankit14082004@gmail.com>
2024-11-16 17:18:27 +0000

    conditional statements

---
+++ basic/conditional.js
@@ -0,0 +1,24 @@
+let color;
+let mode="dark";
+if(mode==="dark"){
+    color="black";
+}
+else{
+    color="white";
+}
+console.log(color);
+
+//conditional statement 
+/*
+    1.if
+    2.if-else
+    3.if-else if
+*/
+
+//ternary operator
+    /*
+        conditon ? statement 1(if true):statement 2(if false statement 1);
+    */
+
+    let a=25;
+    a%2===0? console.log("even"):console.log("odd");
