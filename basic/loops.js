3519032a10a1dbe121cf55c432891d980ccaebf0
Ankit Maurya <ankit14082004@gmail.com>
2024-11-20 15:30:06 +0000

    loops

---
+++ basic/loops.js
@@ -0,0 +1,21 @@
+// loops in javasript
+    /*
+        1.for loop
+            for(intialisaton;condition;updation){
+                statement;
+            }
+    */
+   for(let i=1;i<=8;i=i++){
+    console.log("hello world!");
+   }
+
+    /*
+        infinite loop
+         condition is always true
+         eg. for(let i=0;i>=0;i++){
+                statement;
+                }    */
+    
+   /*
+        
+   */
