c794f3de049dd5f764031366c6e742de7a7162fb
Ankit Maurya <ankit14082004@gmail.com>
2025-08-22 09:56:33 +0000

    operation

---
+++ basic_02/operation.js
@@ -0,0 +1,20 @@
+// 1. arthematic operator
+//+,-,*,/,%,**(power)
+//example 
+// let a=5+5;
+// let b=6-7;
+// let c=4*9;
+// let d=5/2;
+// let e=3%2;
+// let f=3**2
+
+// console.table([a,b,c,d,e]);
+
+
+//2.concatenation
+let str1="hello";
+let str2=" ankit";
+let str3=str1+str2;
+// console.log(str3);
+
+console.log("hello"+" ankit");
