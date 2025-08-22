435d759faf709078d075e23c362eefb0edc59b25
Ankit Maurya <ankit14082004@gmail.com>
2025-08-22 08:56:38 +0000

    primitive

---
+++ basic_02/datatypes.js
@@ -0,0 +1,47 @@
+"use strict";
+// alert(3+3); // use in browser console
+
+//arthematic operator
+//+,-,*,/,%
+//example 
+// let a=5+5;
+// let b=6-7;
+// let c=4*9;
+// let d=6/2;
+// let e=3%2;
+
+// console.table([a,b,c,d,e]);
+
+let a="ankit";
+let age=21;
+let isLogged=false;
+
+
+//1.primitive datatype
+
+// number =>2 to power 53
+// bigint
+// string
+// boolean =>true/false
+// null =>standlone value
+// undefined => 
+// symbol
+
+let num=123;
+let str="string value";
+let flag=true;
+let null_val=null;
+let und;
+let array=[1,2,3];
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof flag);
+console.log(typeof null_val);
+console.log(typeof und);
+console.log(typeof array);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
