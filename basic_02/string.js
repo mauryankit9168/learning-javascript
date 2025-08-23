08819d41c7bfa7142c3c876ccc3a91aec2d8fe56
Ankit Maurya <ankit14082004@gmail.com>
2025-08-23 18:28:36 +0000

    string

---
+++ basic_02/string.js
@@ -0,0 +1,14 @@
+// const name="ankit"
+// const repoCount=5;
+
+// //concatenation
+// //console.log(name+ repoCount+"nice")    //this is old 
+
+// console.log(`hello! my name is ${name}, my repoCount is ${repoCount},thank you!`);
+//use backticks ans dollar sign
+
+//string declaration
+
+const str=new String("ankitMaurya");
+console.log(str.__proto__)
+console.log(str.italics);
